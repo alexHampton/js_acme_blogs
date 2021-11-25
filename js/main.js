1ed1cea9a6d8e87e47549152d5621e09b12b0afb
Alex Hampton <ahampton5158@gmail.com>
2021-11-25 10:37:55 +0900

    Add more functions

--- js/main.js
+++ js/main.js
@@ -5,22 +5,16 @@ const createElemWithText = (element = 'p', text = '', className = '') => {
     return elemWithText;
 }
 
-const getUsers = async () => {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users = await resp.json();
-    console.log(users);
-    return users;
-} 
 
-let myUsers = [
-    { id: 1, name: "Leanne Graham", username: 'Bret', email: "anything@anywhere.com"},
-    { id: 2, name: "Bill Masters", username: "Bob", email: "another"},
-    { id: 3, name: "Joe Namath", username: "Not Joe", email: "the other joe"}
-]
+// let myUsers = [
+//     { id: 1, name: "Leanne Graham", username: 'Bret', email: "anything@anywhere.com"},
+//     { id: 2, name: "Bill Masters", username: "Bob", email: "another"},
+//     { id: 3, name: "Joe Namath", username: "Not Joe", email: "the other joe"}
+// ]
 
 // creates option elements for all users in usersJSON
 const createSelectOptions =  (usersJSON) => {
-    if (!usersJSON) return undefined
+    if (!usersJSON) return
 
     let options = usersJSON.map( user => {
         let optionElement = document.createElement("option")
@@ -34,7 +28,7 @@ const createSelectOptions =  (usersJSON) => {
 // receives a postId and toggles the comment section of that id.
 // returns the section
 const toggleCommentSection = (postId) => {
-    if (!postId) return undefined
+    if (!postId) return
 
     const idSections = document.getElementsByTagName('section')
     const idSection = Array.from(idSections).filter((section) => {
@@ -46,7 +40,7 @@ const toggleCommentSection = (postId) => {
 }
 
 const toggleCommentButton = (postId) => {
-    if (!postId) return undefined
+    if (!postId) return
 
     let buttons = document.getElementsByTagName('button')
     buttons = Array.from(buttons).filter((btn) => {
@@ -63,4 +57,103 @@ const toggleCommentButton = (postId) => {
     return button
 }
 
-toggleCommentButton(1)
+
+// recap: I first started making async functions but didn't need to.
+// It only made things more complicated. I have learned to start basic and 
+// slowly make it as complex as it needs to be. 
+
+// I was having difficulty checking the 'data-post-id' attributes, so I logged
+// my objects to the console so I could inspect them. I also realized that since
+// some objects did not have that property, I had to check if the property exists
+// first.
+
+const deleteChildElements = (parent) => {
+    if (!(parent instanceof HTMLElement)) return
+    let child = parent.lastElementChild
+    while (child) {
+        parent.removeChild(child)
+        child = parent.lastElementChild
+    }
+    return parent
+}
+
+// test function for now
+const toggleComments = (event, id) => {
+
+}
+
+const addButtonListeners = () => {
+    const buttons = document.querySelectorAll('main button')
+    buttons.forEach((btn) => {
+        let postId = btn.dataset.postId
+        btn.addEventListener('click', (event) => {
+            toggleComments(event, postId)
+        })
+    })
+    return buttons
+}
+
+const removeButtonListeners = () => {
+    const buttons = document.querySelectorAll('main button')
+    buttons.forEach(btn => {
+        //let postId = btn.dataset.postId
+        btn.removeEventListener('click', addButtonListeners)
+
+    })
+    return buttons
+}
+
+const createComments = (jsonComments) => {
+    if (!jsonComments) return
+    const fragment = document.createDocumentFragment()
+    jsonComments.forEach((comment) => {
+        let article = document.createElement('article')
+        let h3 = createElemWithText('h3', comment.name)
+        let p = createElemWithText('p', comment.body)
+        let pEmail = createElemWithText('p', `From: ${comment.email}`)
+        article.appendChild(h3)
+        article.appendChild(p)
+        article.appendChild(pEmail)
+        fragment.appendChild(article)
+    })
+    return fragment
+}
+
+const populateSelectMenu = (jsonUsers) => {
+    if (!jsonUsers) return
+    const menu = document.getElementById('selectMenu')
+    const options = createSelectOptions(jsonUsers)
+    options.forEach(option => menu.appendChild(option))
+    return menu
+}
+
+const getUsers = async () => {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users = await resp.json();
+    return users;
+}
+
+const getUserPosts = async (postId) => {
+    if (!postId) return
+    try {
+        const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/?userId=${postId}`)
+        const posts = await resp.json()
+        return posts
+    } catch (err) {
+        console.log(err)
+    }   
+}
+
+const getUser = async (userId) => {
+    if (!userId) return
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    const user = await resp.json()
+    return user
+}
+
+const getPostComments = async (postId) => {
+    if (!postId) return
+    const resp = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+    const comments = await resp.json()
+    return comments
+}
